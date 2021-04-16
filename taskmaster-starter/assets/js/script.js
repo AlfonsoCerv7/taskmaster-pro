82d4042e5769b96fca7e1a04ecf1689edd74a4b6
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-04-16 13:48:41 -0500

    added time intervals for auditing and custom fonts

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -270,3 +270,9 @@ var auditTask = function(taskEl) {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
